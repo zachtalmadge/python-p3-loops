30931bc98f0b1d947b3bdf91bec209605900d3e9
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-22 18:48:51 -0400

    pass tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        if i == 1:
+            print('Happy New Year!')
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x ** 2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if (i % 3 == 0 and i % 5 == 0):
+            print('FizzBuzz')
+        elif (i % 3 == 0):
+            print('Fizz')
+        elif ( i % 5 == 0):
+            print('Buzz')
+        else:
+            print(i)
+        i += 1
